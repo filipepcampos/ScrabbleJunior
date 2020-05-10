3f15aba05bc7a5a75ff88987df36b895f41d70ba
VascoG8 <64217781+VascoG8@users.noreply.github.com>
2020-05-10 22:43:43 +0100

    Update Pool.cpp

--- Pool.cpp
+++ Pool.cpp
@@ -1,44 +1,18 @@
-#include <iostream>
 #include "Pool.h"
-Pool::Pool(const std::vector <char> &letters)
-{
-	allLetters = letters;
-	int number;
-	for (int i = 0; i < 7; i++)
-	{
-		number = rand() % (allLetters.size());
-		lettersBag.push_back(allLetters.at(number));
-		allLetters.erase(allLetters.begin()+number);
-	}
-}
-Pool::~Pool()
-{
-	lettersBag.clear();
-}
-std::vector<char> Pool::getLettersBag()const
-{
-	return lettersBag;
-}
-std::vector<char> Pool::getAllLetters()const
-{
-	return allLetters;
-}
-void Pool::removeLetter(char letter)
-{
-	lettersBag.erase(std::find(lettersBag.begin(),lettersBag.end(),toupper(letter)));
-}
-void Pool::addLetter()
-{
-	int number = rand() % (allLetters.size());
-	lettersBag.push_back(allLetters.at(number));
-	allLetters.erase(allLetters.begin() + number);
 
-}
-void Pool::showBag()const
+Pool::Pool(const std::vector <char>& letters)
 {
-	for (int i = 0; i < 7; i++)
-	{
-		std::cout << lettersBag[i] << ',';
-	}
-	std::cout << std::endl;
+    allLetters = letters;
+}
+char Pool::draw()
+{
+    if (allLetters.size() != 0)
+    {
+        int number = rand() % (allLetters.size());
+        char c = allLetters.at(number);
+        allLetters.erase(allLetters.begin() + number);
+        return c;
+    }
+    else
+        return ' ';
 }
