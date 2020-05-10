879de69d1e88c515280e3a8459f46d491ee56640
VascoG8 <64217781+VascoG8@users.noreply.github.com>
2020-05-10 22:44:05 +0100

    Update Pool.h

--- Pool.h
+++ Pool.h
@@ -1,23 +1,18 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <limits>
+#include <iostream>
 
 #ifndef POOL_H
 #define POOL_H
 
 class Pool
 {
-public:
-	Pool(const std::vector <char>& allLetters);
-	~Pool();
-	std::vector<char> getLettersBag()const;
-	std::vector<char> getAllLetters()const;
-	void showBag();
-	void removeLetter(char letter);
-	void addLetter();
 private:
-	std::vector <char> lettersBag;
-	std::vector <char> allLetters;
+    std::vector<char> allLetters;
+public:
+    explicit Pool(const std::vector<char>& letters);
+    char draw();
 };
-
 #endif
